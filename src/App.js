65a25ac444c040f013808f2136d32e002d3b6128
Lario <osunkunleolanrewaju@gmail.com>
2021-03-17 04:03:18 +0100

    build the footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css';
+
+import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Footer />
     </div>
   );
 }
